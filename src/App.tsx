6e77f8cac1a05e84fe4d11d0c4cdc4fd1d98720a
lokhmakov <l.pavel.m@gmail.com>
2022-09-03 13:35:25 +0300

    4 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import * as A from './cva/button';
 
 function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+  return <A.Screen />;
 }
 
 export default App;
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div className="flex h-screen">
-      <div className="m-auto">
-        <div className="text-6xl text-red-600">{count}</div>
-        <button
-          className="px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white"
-          type="button"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count+
-        </button>
-      </div>
-    </div>
-  );
-};
---
+++ src/cva/button.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { cva } from 'class-variance-authority';
+import type { VariantProps } from 'class-variance-authority';
+
+const button = cva(['font-semibold', 'border', 'rounded'], {
+  variants: {
+    intent: {
+      primary: [
+        'bg-blue-500',
+        'text-white',
+        'border-transparent',
+        'hover:bg-blue-600',
+      ],
+      secondary: [
+        'bg-white',
+        'text-gray-800',
+        'border-gray-400',
+        'hover:bg-gray-100',
+      ],
+    },
+    size: {
+      small: ['text-sm', 'py-1', 'px-2'],
+      medium: ['text-base', 'py-2', 'px-4'],
+    },
+  },
+  compoundVariants: [{ intent: 'primary', size: 'medium', class: 'uppercase' }],
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
+  },
+});
+
+export const Button = (
+  props: VariantProps<typeof button> & { children: React.ReactNode }
+) => {
+  return <button className={button(props)} {...props} />;
+};
+
+export function Screen() {
+  return (
+    <>
+      <Button size="small">Small</Button>
+      <Button>Medium</Button>
+    </>
+  );
+}
