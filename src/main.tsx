5f5f65574f8ee16537e651cfd2ca73c36413635a
lokhmakov <l.pavel.m@gmail.com>
2022-09-05 10:13:12 +0300

    1 file modified

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { render } from 'react-dom';
 import { Routes } from 'generouted';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -6,7 +5,5 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <Routes />
-  </React.StrictMode>
 );
