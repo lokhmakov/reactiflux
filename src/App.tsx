319bd624652b48e26bc531c9e9f5cd76efc5c56a
lokhmakov <l.pavel.m@gmail.com>
2022-09-05 10:26:36 +0300

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,3 @@ function App() {
 }
 
 export default App;
-
-const superLongArray = [1, 2, 3];
-
-async function main() {
-  for await (const i of superLongArray) {
-    console.log(i);
-  }
-}
-
-main(); // => 1, 2, 3
-
-Object.assign(superLongArray, {
-  async *[Symbol.asyncIterator]() {
-    yield 10;
-    yield 9;
-    yield 8;
-  },
-});
-
-main(); // => 10, 9, 8
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { Routes } from 'generouted';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
