ee32bd590a5aeb4247a1841cf6553ea9216e0cb1
lokhmakov <l.pavel.m@gmail.com>
2022-09-06 18:20:18 +0300

    1 file added

---
+++ src/pages/local-storage/01.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+Object.defineProperty(window, 'localStorage', {
+  configurable: true,
+  enumerable: true,
+  value: new Proxy(localStorage, {
+    get: function (storage, key) {
+      if (key === `setItem`) {
+        return (...args) => {
+          console.log(`setItem called`, args);
+          storage.setItem.apply(storage, args);
+        };
+      }
+
+      return storage[key].bind(storage);
+    },
+  }),
+});
+
+localStorage.setItem('foo', 'bar');
+
+export default function Page() {
+  return (
+    <div className="grid gap-2">
+      <button
+        className="p-1 bg-green-400 rounded-lg"
+        onClick={() => localStorage.setItem(`key`, `value`)}
+      >
+        Change Me
+      </button>
+    </div>
+  );
+}
