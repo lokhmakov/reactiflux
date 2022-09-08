fddf92ee8c26dab5f1338676a576b85a504664a3
lokhmakov <l.pavel.m@gmail.com>
2022-09-08 06:29:39 +0000

    .

---
+++ src/pages/2022-09-08/01-Dumb.jsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+
+export default function Page() {
+  const [list, setList] = React.useState(() => data)
+
+  function _onClick(id) {
+    setList((prevList) => prevList.filter((v) => v.id !== id))
+  }
+
+  return list.map(({id, name}) => (
+    <Item key={id} name={name} onClick={() => _onClick(id)} />
+  ))
+}
+
+const Item = ({name, onClick}) => <div onClick={onClick}>{name}</div>
+
+const data = [
+  {
+    id: 1,
+    name: 'Harry Styles',
+    age: 24,
+    img: null,
+  },
+
+  {
+    id: 2,
+    name: 'Jagdamba Banaraswale',
+    age: 89,
+    img: null,
+  },
+  {
+    id: 3,
+    name: 'Bunty bhutia',
+    age: 33,
+    img: null,
+  },
+  {
+    id: 4,
+    name: 'Chinku jhunjhunwala',
+    age: 19,
+    img: null,
+  },
+  {
+    id: 5,
+    name: 'Simran Kataria',
+    age: 29,
+    img: null,
+  },
+]
