c669ab991f9461a74869923afae7c87444262f6e
lokhmakov <l.pavel.m@gmail.com>
2022-09-24 16:51:02 +0300

    .

--- src/pages/issues/2022-09-24/02-itskvant.jsx
+++ src/pages/issues/2022-09-24/02-itskvant.jsx
@@ -14,10 +14,19 @@ export default function Page() {
     )
   }
 
+  function onRemove(id) {
+    setCards((prev) => prev.filter((item) => item.id !== id))
+  }
+
   return (
     <div className="grid grid-cols-3 gap-2 p-2 select-none cursor-pointer">
       {cards.map((card) => (
-        <Card key={card.id} {...card} onClick={() => onClick(card.id)} />
+        <Card
+          key={card.id}
+          {...card}
+          onClick={() => onClick(card.id)}
+          onRemove={() => onRemove(card.id)}
+        />
       ))}
     </div>
   )
@@ -27,12 +36,16 @@ function Card(props) {
   return (
     <div
       className={cn(
-        `rounded-xl p-2`,
+        `rounded-xl p-2 flex justify-between`,
         props.selected ? `bg-red-200` : `bg-green-200`,
       )}
       onClick={props.onClick}
     >
       {props.genre}
+      <div
+        className="rounded bg-black text-white w-4 h-4 text-xs"
+        onClick={props.onRemove}
+      />
     </div>
   )
 }
