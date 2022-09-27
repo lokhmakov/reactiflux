10d13ffb010b010349cdf56c7b449bab20d2e75f
lokhmakov <l.pavel.m@gmail.com>
2022-09-27 10:57:40 +0300

    .

--- src/pages/issues/2022-09-24/02-itskvant.jsx
+++ src/pages/issues/2022-09-24/02-itskvant.jsx
@@ -6,46 +6,46 @@ export default function Page() {
     Array.from({length: 10}, (_, id) => ({id, genre: `genre ${id}`})),
   )
 
-  function onClick(id) {
-    setCards((prev) =>
-      prev.map((item) =>
-        item.id === id ? {...item, selected: !item.selected} : item,
+  const onClick = React.useCallback(
+    (id) =>
+      setCards((prev) =>
+        prev.map((item) =>
+          item.id === id ? {...item, selected: !item.selected} : item,
+        ),
       ),
-    )
-  }
+    [],
+  )
 
-  function onRemove(id) {
-    setCards((prev) => prev.filter((item) => item.id !== id))
-  }
+  const onRemove = React.useCallback(
+    (id) => setCards((prev) => prev.filter((item) => item.id !== id)),
+    [],
+  )
 
   return (
     <div className="grid grid-cols-3 gap-2 p-2 select-none cursor-pointer">
       {cards.map((card) => (
-        <Card
-          key={card.id}
-          {...card}
-          onClick={() => onClick(card.id)}
-          onRemove={() => onRemove(card.id)}
-        />
+        <Card key={card.id} card={card} onClick={onClick} onRemove={onRemove} />
       ))}
     </div>
   )
 }
 
-function Card(props) {
+const Card = React.memo(({card, onClick, onRemove}) => {
+  console.log(`render`)
+
   return (
     <div
       className={cn(
         `rounded-xl p-2 flex justify-between`,
-        props.selected ? `bg-red-200` : `bg-green-200`,
+        card.selected ? `bg-red-200` : `bg-green-200`,
       )}
-      onClick={props.onClick}
+      onClick={() => onClick?.(card.id)}
     >
-      {props.genre}
+      {card.genre}
       <div
         className="rounded bg-black text-white w-4 h-4 text-xs"
-        onClick={props.onRemove}
+        onClick={() => onRemove?.(card.id)}
       />
     </div>
   )
-}
+})
