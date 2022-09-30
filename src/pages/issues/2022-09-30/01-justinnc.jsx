fe32604b02743d20c8e999e2c6f61421c48da274
lokhmakov <l.pavel.m@gmail.com>
2022-09-30 11:53:45 +0300

    .

---
+++ src/pages/issues/2022-09-30/01-justinnc.jsx
@@ -0,0 +1,15 @@
+const sum = (...args) => args.reduce((a, b) => a + b)
+
+// Number.prototype.sum = function (...args) {
+//   return sum(this, ...args)
+// }
+
+// Number.prototype.sum = function (...args) {
+//   return args.reduce((a, b) => a + b, this)
+// }
+
+Number.prototype.sum = function (n) {
+  return this + n
+}
+
+console.log((5).sum(5).sum(10)) // => 20
