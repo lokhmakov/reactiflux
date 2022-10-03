64f511761afce2764538db9772c1512da38df635
lokhmakov <l.pavel.m@gmail.com>
2022-10-03 13:23:54 +0300

    .

--- src/pages/issues/2022-10/03/04.jsx
+++ src/pages/issues/2022-10/03/04.jsx
@@ -3,7 +3,9 @@ const range = (a, b) => Array.from({length: b - a + 1}, (_, i) => a + i)
 console.log(range(10, 20))
 
 function* gRange(a, b) {
-  while (a < b + 1) yield a++
+  while (a <= b) yield a++
 }
 
-console.log([...gRange(10, 20)])
+const range2 = (a, b) => [...gRange(a, b)]
+
+console.log(range2(10, 20))
