428d70ab464dfd99de7247af4ef54f42e5ad11ba
lokhmakov <l.pavel.m@gmail.com>
2022-11-08 21:33:31 +0200

    .

--- src/pages/issues/2022-11/08/01-ortunado.jsx
+++ src/pages/issues/2022-11/08/01-ortunado.jsx
@@ -6,14 +6,30 @@ import * as React from 'react'
 //     `${selectorParent} > ${selector}`,
 //   ].reduce((r, v) => r.concat(...(document.querySelectorAll(v) || [])), [])
 
-const querySelectorWithDepth = (selectorParent, selector, not = ':not(form)') =>
-  document.querySelectorAll(
-    `${selectorParent} > ${not} ${selector}, ${selectorParent} > ${selector}`,
-  )
+// const querySelectorWithDepth = (selectorParent, selector, not = ':not(form)') =>
+//   document.querySelectorAll(
+//     `${selectorParent} ${not} ${selector}, ${selectorParent} > ${selector}`,
+//   )
+
+function* evaluteNodes(result, node) {
+  while ((node = result.iterateNext())) yield node
+}
+
+const evaluate = () => [
+  ...evaluteNodes(
+    document.evaluate(
+      `//form[@data-form="Form1"]//input[not(ancestor::form[@data-form="Form2"])]`,
+      document,
+      null,
+      XPathResult.ANY_TYPE,
+      null,
+    ),
+  ),
+]
 
 export default function App() {
   React.useEffect(() => {
-    console.log(querySelectorWithDepth(`[data-form="Form1"]`, `input`))
+    console.log(evaluate())
   }, [])
 
   return (
@@ -28,12 +44,16 @@ export default function App() {
       <div>
         <input id="i2" />
       </div>
-      <form className=".form" data-form="Form2">
+      <div>
         <div>
-          <input id="i3" />
+          <form className=".form" data-form="Form2">
+            <div>
+              <input id="i3" />
+            </div>
+            <input id="i4" />
+          </form>
         </div>
-        <input id="i4" />
-      </form>
+      </div>
       <input id="i5" />
     </form>
   )
